b6d4b6017fc68fa361dbe58db63f3cf17525e2d0
akashveda536-dot <akashveda536@gmail.com>
2026-06-04 16:16:06 +0530

    Add files via upload

---
+++ akash.c
@@ -0,0 +1,19 @@
+ # include <stdio.h>
+
+ int main()
+{
+ int a,b;
+ printf ("Enter a:");
+ scanf("%d",&a);
+ 
+ printf(" Enter b:");
+ scanf("%d",&b);
+
+ printf("\naddition = %d", a + b);
+ printf("\nsubtraction = %d", a - b);
+ printf("\nmultiplication = %d",a * b);
+ printf("\ndivison = %d",a / b);
+
+ return 0;
+}
+
