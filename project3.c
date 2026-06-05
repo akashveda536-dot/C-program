afa979f799582aa05bf1a69938b0eb309133915d
akashveda536-dot <akashveda536@gmail.com>
2026-06-05 10:50:41 +0530

    Add files via upload

---
+++ project3.c
@@ -0,0 +1,24 @@
+
+
+#include<stdio.h>
+int main()
+{
+ int age;
+ printf("Enter age:");
+ scanf("%d",&age);
+ char name;
+ printf("Enter name:");
+ scanf("%s",&name);
+ int mark;
+ printf("Enter mark:");
+ scanf("%d",&mark);
+if(mark >=50)
+  {
+   printf("passed the exam");
+  }
+ else
+  {
+  printf("failed the exam");
+  }
+return 0;
+}
