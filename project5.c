07967d12b50bd6cbc637ed8d33fdafeccb4a68f4
akashveda536-dot <akashveda536@gmail.com>
2026-06-06 08:13:33 +0530

    Add files via upload

---
+++ project5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int mark;
+ printf("Enter the mark:");
+ scanf("%d",&mark);
+ if(mark >=50)
+ {
+  printf("you have been passed");
+ }
+ else
+ {
+  printf("you have been failed");
+ }
+return 0;
+}
