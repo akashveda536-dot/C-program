c69d2bc6a624539484a12fa8cf02bd80b9744110
akashveda536-dot <akashveda536@gmail.com>
2026-06-08 20:38:29 +0530

    Add files via upload

---
+++ pro11.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+ 
+for(i=1;i>=1;i++)
+  { 	 
+  if(i==1)
+  {   
+   printf("%d\n",i);
+  }
+  else if(i==12)
+  {
+   printf("%d\n",i);
+  }	  
+  else if(i==123)
+  {
+   printf("%d\n",i);
+  }	  
+  else if(i==1234)
+  {
+   printf("%d\n",i);
+   break;
+  }
+  }
+  return 0; 
+}
