eb858f1d9b343f33082fec133209aa7d187df96d
akashveda536-dot <akashveda536@gmail.com>
2026-06-11 10:18:30 +0530

    Add files via upload

---
+++ D23.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int 1;
+	printf("%zu\n",sizeof(1));
+	printf("%zu\n",sizeof(float));
+	printf("%zu\n",sizeof(double));
+	printf("%zu\n",sizeof(int));
+	return 0;
+}
