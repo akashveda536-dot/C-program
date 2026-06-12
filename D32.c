c4242c4778c5a29fed8c30e6eef2d88a376c7b76
akashveda536-dot <akashveda536@gmail.com>
2026-06-12 09:17:35 +0530

    Add files via upload

---
+++ D32.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float f;
+ int i;
+ printf("Enter a float value:");
+ scanf("%f\n",&f);
+ i=(int)f;
+ printf("float value:%f\n",f);
+ printf("int value:%d\n",i);
+ return 0;
+}
+		 
