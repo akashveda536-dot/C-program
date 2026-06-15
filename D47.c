affa67c6c1078ac778dffa1e4e838f52780b4f8e
akashveda536-dot <akashveda536@gmail.com>
2026-06-15 09:25:32 +0530

    Add files via upload

---
+++ D47.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=15,b=4;
+	printf("sum =%d\n",a+b);
+	printf("Difference=%d\n",a-b)
+	            if(a>b)
+		{
+			printf("a is greater thann b\n");
+		}
+	return 0;
+}
